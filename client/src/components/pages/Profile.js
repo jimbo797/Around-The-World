2b0c01fc714e4563f1fa1ef5a1fd2a8ae1ac06fb
camila-zavala <alessandra050505@gmail.com>
2024-01-19 19:51:52 -0500

    merged profile chnages with jimmys

--- client/src/components/pages/Profile.js
+++ client/src/components/pages/Profile.js
@@ -1,55 +1,44 @@
 import React from "react";
 
 import { useState, useEffect } from "react";
+import { socket } from "../../client-socket.js";
 import "../modules/Background";
 import { get, post } from "../../utilities.js";
 import Background from "../modules/Background";
 import Page from '../modules/Page'
 
 const Profile = ({userId}) => {
+
+  if (props.userId === undefined){
+    return <NotLoggedInPage/> //need to make this page
+  }
+
+  const [username, setUsername] = useState("");
+  const getUser =() => {
+    // the "sub" field means "subject", which is a unique identifier for each user
+    get("/api/getUsername").then((data) =>{
+      return data.username;
+    });
+  };
+
+  socket.on("username",(data) => setUsername(data));
+
+
+  useEffect(() => {
+    get("/api/getUsername").then((data) => setUsername(data.username));
+  }, []);
+
+  const changeUsername = () => {
+    let body = { username: message };
+    post("/api/changeUsername", body).then((res) => {
+      console.log(res.message);
+    });
+  };
   return (
     <Page userId={userId}>
-
+    props.userId
     </Page>
   );
 };
-// import { socket } from "../../client-socket.js";
-
-// const ProfilePage = (props) => {
-
-//   if (props.userId === undefined){
-//     return <NotLoggedInPage/>
-//   }
-
-//   const [username, setUsername] = useState("");
-//   const getUser =() => {
-//     // the "sub" field means "subject", which is a unique identifier for each user
-//     get("/api/getUsername").then((data) =>{
-//       return data.username;
-//     });
-//   };
-
-//   socket.on("username",(data) => setUsername(data));
-
-
-//   useEffect(() => {
-//     get("/api/getUsername").then((data) => setUsername(data.username));
-//   }, []);
-
-//   const changeUsername = () => {
-//     let body = { username: message };
-//     post("/api/changeUsername", body).then((res) => {
-//       console.log(res.message);
-//     });
-//   };
-
-//   return (
-//     <div>
-//       <Background>
-//         <h1>hi</h1>
-//       </Background>
-//     </div>
-//   );
-// };
 
 export default Profile;
