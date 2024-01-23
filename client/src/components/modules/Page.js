2c5fec3d4409489a5b3f02119daf7e19631a1bc8
camila-zavala <alessandra050505@gmail.com>
2024-01-23 16:47:23 -0500

    background

--- client/src/components/modules/Page.js
+++ client/src/components/modules/Page.js
@@ -24,7 +24,7 @@ const Page = ({ userId, children }) => {
     <div className="u-absolute page-size u-flexRow page-overflow">
       <NavBar userId={userId}></NavBar>
       <Background>
-        <div className="u-flexRow page-username-padding">
+        <div className="u-flexRow page-text">
           <div>Online Passport</div>
           <div className="u-justifyRight">{message}</div>
         </div>
