d470b5d06bb23a79f8da165e4d33e88a9bfca234
miranda-liu <47536718+miranda-liu@users.noreply.github.com>
2024-01-25 01:20:56 -0800

    home page from figma

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,22 +1,44 @@
-import React, { useState, useEffect } from "react";
-import Page from "../modules/Page";
-import "./Home.css";
+// import React, { useState, useEffect } from "react";
+// import Page from "../modules/Page";
+// import "./Home.css";
 
-const Home = ({ userId }) => {
-    // if (userId === undefined){
-    //   return <NotLoggedInPage/> //need to make this page
-    // }
+// const Home = ({ userId }) => {
+//     // if (userId === undefined){
+//     //   return <NotLoggedInPage/> //need to make this page
+//     // }
   
-    return (
-      <div>
-        {/* {adding images via link for imgur: https://apidocs.imgur.com/} */}
-        {/* <img 
-        src="https://worldanimalfoundation.org/wp-content/uploads/2023/09/Cute-dogs.jpg"
-        alt="new"
-        /> */}
-        <p>Hi</p>
+//     return (
+//       <div>
+//         {/* {adding images via link for imgur: https://apidocs.imgur.com/} */}
+//         {/* <img 
+//         src="https://worldanimalfoundation.org/wp-content/uploads/2023/09/Cute-dogs.jpg"
+//         alt="new"
+//         /> */}
+//         <p>Hi</p>
+//       </div>
+//     );
+//   };
+  
+//   export default Home;
+
+import React, { memo } from 'react';
+
+import '../_resets.module.css';
+import './Home.css';
+
+const Home = memo(function Desktop1(props = {}) {
+  return (
+    <div className="clapyResets root">
+      <div className="prioritize">
+        <div className="travelAnywhere">travel anywhere.</div>
+        <div className="rectangle20"></div>
+        <div className="startTraveling">start traveling</div>
+        <div className="aroundTheWRld11"></div>
+        <div className="aROUNDTHEWORLD">AROUND THE WORLD</div>
       </div>
-    );
-  };
-  
-  export default Home;
+      <div className="traveling1"></div>
+    </div>
+  );
+});
+
+export default Home;
