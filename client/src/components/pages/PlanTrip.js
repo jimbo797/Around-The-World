cce6441177df3e7d7948b4758ca55a276d2b5fb4
camila-zavala <alessandra050505@gmail.com>
2024-01-26 00:50:44 -0500

    inital layout for plan a trip, similar to chat gpt, can play around with font and design later

--- client/src/components/pages/PlanTrip.js
+++ client/src/components/pages/PlanTrip.js
@@ -4,7 +4,36 @@ import axios from "axios";
 import "./PlanTrip.css";
 
 const PlanTrip = () => {
-  return <Page userId={userId}></Page>;
+
+  return (
+    <div className="background">
+      <section className="chat-side-bar">
+        <button>New Trip</button>
+        <ul className="past-trips">
+          <li>a trip</li>
+        </ul>
+        <nav>
+          <p>Around the World</p>
+        </nav>
+      </section>
+      <section className="main">
+        <h1> Plan a Trip </h1>
+        <ul className="feed">
+
+        </ul>
+        <div className="bottom">
+          <div className="input">
+            <input/>
+            <div id="submit">✈</div>
+          </div>
+          <p className="information">
+            We hope planning your next adventure is made easier with 
+            Around the World.
+          </p>
+        </div>
+      </section>
+    </div>
+  )
 };
 
 export default PlanTrip;
