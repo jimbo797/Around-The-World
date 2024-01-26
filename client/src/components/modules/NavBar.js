be835b61135821128ad1a21eb2badd11f0dae0f6
miranda-liu <47536718+miranda-liu@users.noreply.github.com>
2024-01-26 10:41:09 -0800

    added logo to navbar

--- client/src/components/modules/NavBar.js
+++ client/src/components/modules/NavBar.js
@@ -2,15 +2,17 @@ import React from "react";
 
 import "./NavBar.css";
 import "../../utilities.css";
+// import "../pages/Home.css";
 
 const NavBar = ({ userId }) => {
   const pages = [
-    { name: "Online Passport", link: "/" },
+    // { name: "AROUND THE WORLD", link: "/" },
     { name: "Feed", link: "/feed" },
     { name: "Profile", link: "/profile" },
     { name: "Plan a Trip", link: "/plantrip" },
     { name: "Settings", link: "/settings" },
   ];
+
   const navbarLinks = pages.map(({ name, link }) => (
     <a href={link} key={name}>
       <div className="navbar-text-color navbar-text-padding navbar-text-style">{name}</div>
@@ -20,6 +22,12 @@ const NavBar = ({ userId }) => {
   return (
     <div className="navbar-size navbar-bgcolor float-left navbar-position">
       {/* <h1 className="page-text">Online Passport</h1> */}
+      {/* <img className="navbar-logo" src="assets/logo.png"/> */}
+      {/* <div className="NavBar-logoImage"/> */}
+      {/* <div> {logoLink} </div> */}
+      <a href="/">
+        <img className="navbar-logo" src="/assets/logo.png" alt="Your Image"/>
+      </a>
       <div className="navbar-centering">{navbarLinks}</div>
     </div>
   );
