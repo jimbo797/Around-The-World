ef7722502ed52f4e5800c1f11cf52e809087b29b
miranda-liu <47536718+miranda-liu@users.noreply.github.com>
2024-01-29 13:57:25 -0800

    updated unfollowing

--- client/src/components/modules/RemoveUserProfile.js
+++ client/src/components/modules/RemoveUserProfile.js
@@ -9,6 +9,7 @@ import { post } from "../../utilities";
  * @param {string} _id of the story
  * @param {string} name of user
  * @param {string} googleid of user
+ * @param {string} locations of user
  * @param {string} unfollowUser true/false for if user has followed this person
  */
 const RemoveUserProfile = (props) => {
@@ -32,6 +33,39 @@ const RemoveUserProfile = (props) => {
     //   props.followUser(user);
     // });
   };
+  function capitalizeFirstLetter(city) {
+    // Check if the city is not an empty string or null
+    if (city && typeof city === 'string') {
+      // Capitalize the first letter of each word in the string
+      return city
+        .split(' ')
+        .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+        .join(' ');
+    } else {
+      // Return the original value for non-string or empty values
+      return city;
+    }
+  }
+
+  function capitalizeCityNames(cityList) {
+    // Check if the input is an array
+    if (Array.isArray(cityList)) {
+      // Capitalize each city name using the capitalizeFirstLetter function
+      return cityList.map(capitalizeFirstLetter);
+    } else {
+      // Return the original value for non-array input
+      return cityList;
+    }
+  }
+
+  function formatCityNames(cityList){
+   
+    for ( let i = 0; i < cityList.length - 1; i++){
+      cityList[i] = cityList[i] + ", ";
+    }
+
+    return cityList
+  }
 
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -47,7 +81,7 @@ const RemoveUserProfile = (props) => {
   return (
     <div className="Card-story">
       <p className="">{props.name}</p>
-      <p className="">{props.googleid}</p>
+      <p className="">Visited: {formatCityNames(capitalizeCityNames(props.locations))}</p>
       <button
         type="submit"
         className="NewPostInput-button u-pointer"
